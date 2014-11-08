6e5de88f65d0e0f72e393b91be671a9179ad22ba
Hallbjorn <hthg5@hi.is>
2014-11-08 15:47:53 +0000

    Try to account for scvs that have orders to build sd.

--- FirstBotSC/BuildSecondBarracks.cpp
+++ FirstBotSC/BuildSecondBarracks.cpp
@@ -20,17 +20,24 @@ Advice BuildSecondBarracks::GetAdvice()
     } else if (GetCurrent() >= 18) {
         if(GetMinerals() >= 100) {
 			int countBarracksUnderConstruction = 0; // Take care of this here for now.
+			int countSCVsGoingToBuildBarracks = 0; // Take care of this here for now.
 			for (auto unit : BWAPI::Broodwar->self()->getUnits()) {
 				if (!unit->exists()) {
 					continue;
 				}
 
-				if (unit->getType() == BWAPI::UnitTypes::Terran_SCV && unit->getBuildType() == BWAPI::UnitTypes::Terran_Barracks) {
-					countBarracksUnderConstruction++;
+				if (unit->getType() == BWAPI::UnitTypes::Terran_SCV) {
+					if (unit->getBuildType() == BWAPI::UnitTypes::Terran_Barracks) {
+						countBarracksUnderConstruction++;
+					} else if (unit->getLastCommand().getType() == BWAPI::UnitCommandTypes::Build
+							&& unit->getLastCommand().getUnitType() == BWAPI::UnitTypes::Terran_Barracks) {
+						// Naive accounting for SCVs that have possible been commanded to build barracks but not yet started.
+						countSCVsGoingToBuildBarracks++;
+					}
 				}
 			}
 
-			if (countBarracksUnderConstruction + GetUnitCount(UnitTypeEnum::Terran_Barracks) < 2) {
+			if (countSCVsGoingToBuildBarracks + countBarracksUnderConstruction + GetUnitCount(UnitTypeEnum::Terran_Barracks) < 2) {
 				return BuildBarracks;
 			}
         }
